aacb31f195110f419b3f6f321489639f05ad65ec
GaoXiong <xiong.gao@shopee.com>
2024-07-15 12:06:07 +0800

    chore: dev

--- example/03_123e3.test.ts
+++ example/03_123e3.test.ts
@@ -1,19 +1,15 @@
 import {describe, expect, test} from '@jest/globals';
 import { parse } from '../lib'
 
-const data = `{"v1":1e-24,"v2":123000000000000000,"v3":1.234e21}`
+const data1 = `{"v1":1e-24,"v2":123000000000000000,"v3":1.234e21}`
+const data2 = `{"v1":1e-24,"v2":"123000000000000000","v3":"1.234e21"}`
 
 // const a = 1.2345678901234568e21;
 // const b = 1234567890123456789012;
 // const c = 1.2345678901234568e+21;
 
-console.log(JSON.parse(data))
-console.log(parse(data))
-
 describe('sum module', () => {
   test('test 03', () => {
-
-    expect(3).toBe(3);
-    // expect(JSON.stringify(parse(data))).toBe(val);
+    expect(JSON.stringify(parse(data1))).toBe(data2);
   });
 });
